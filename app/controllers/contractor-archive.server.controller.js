bf73c9d6b85aa0db92bcce802b21924dbb660252
Victor Rogers <victor.r.rogers@gmail.com>
2015-01-30 01:41:31 -0600

    Completed server-side contractors controller and added the front-end contractors module

--- app/controllers/contractor-archive.server.controller.js
+++ app/controllers/contractor-archive.server.controller.js
@@ -4,39 +4,98 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-    _ = require('lodash');
+		errorHandler = require('./errors.server.controller'),
+		ContractorArchive = mongoose.model('ContractorArchive'), 
+		_ = require('lodash');
 
 /**
  * Create a Contractor archive
  */
 exports.create = function(req, res) {
+	var contractorArchive = new ContractorArchive(req.body);
 
+	contractor.save(function(err) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			var socketio = req.app.get('socketio');
+			socketio.sockets.emit('contractorArchive.created', contractorArchive);
+			res.json(contractorArchive);
+		}
+	});
 };
 
 /**
  * Show the current Contractor archive
  */
 exports.read = function(req, res) {
-
+	res.json(req.contractorArchive);
 };
 
 /**
  * Update a Contractor archive
  */
 exports.update = function(req, res) {
+	var contractorArchive = req.contractorArchive;
 
+	contractorArchive = _.extend(contractorArchive, req.body);
+
+	contractor.save(function(err) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			var socketio = req.app.get('socketio');
+			socketio.sockets.emit('contractorArchive.updated', contractorArchive);
+			res.json(contractorArchive);
+		}
+	});
 };
 
 /**
  * Delete an Contractor archive
  */
 exports.delete = function(req, res) {
+ var contractorArchive = req.contractorArchive;
 
+ contractorArchive.remove(function(err) {
+	 if (err) {
+		 return res.status(400).send({
+			 message:errorHandler.getErrorMessage(err)
+		 });
+	 } else {
+		 var socketio = req.app.get('socketio');
+		 socketio.sockets.emit('contractorArchive.deleted', contractorArchive);
+		 res.json(contractorArchive);
+	 }
+ });
 };
 
 /**
  * List of Contractor archives
  */
 exports.list = function(req, res) {
+	ContractorArchive.find().sort('-StartDate').populate('user', 'displayName')
+	.exec(function(err, contractorArchive) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			res.json(contractorArchive);
+		}
+	});
+};
 
+exports.contractorArchiveByID = function(req, res, next, id) {
+	ContractorArchive.findById(id).populate('user', 'displayName')
+	.exec(function(err, contractorArchive) {
+		if (err) return next(err);
+		if (!contractorArchive) return next(new Error('Failed to load archive ' + id));
+		req.contractorArchive = contractorArchive;
+		next();
+	});
 };
