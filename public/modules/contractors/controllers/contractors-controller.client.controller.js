a3d9e65e393f9a073c4a5285b0ccd75ca58f6249
Victor Rogers <victor.r.rogers@gmail.com>
2015-01-30 12:52:27 -0500

    Added front-end contractors controller

---
+++ public/modules/contractors/controllers/contractors-controller.client.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('contractors').controller('ContractorsControllerController',
+		['$scope', '$stateParams', '$location', '$modal', '$log',
+		 'Users', 'Socket', 'Authentication', 'SentryActivity',
+	function($scope, $stateParams, $location, $modal, $log,
+					 Users, Socket, Authentication) {
+
+	}
+]);
